a997c5c9c4d7f741c105c41292edc54056bf6787
Jessy Catterwaul <jessy@catterwaul.com>
2024-07-21 17:22:16 -0400

    SIMD3.isBetween

--- Sources/FloatingPoint/Vector/SIMD3+FloatingPointVector.swift
+++ Sources/FloatingPoint/Vector/SIMD3+FloatingPointVector.swift
@@ -1,3 +1,13 @@
 extension SIMD3: FloatingPointVector where Scalar: FloatingPointScalar {
   @inlinable public var normalized: Self { Scalar.normalize(self) }
 }
+
+// MARK: - public
+public extension SIMD3 where Self: FloatingPointVector {
+  @inlinable func isBetween(_ vector0: Self, and vector1: Self) -> Bool {
+    let localProjection = Scalar.Matrix3x2(
+      Scalar.Matrix3x3(vector0, vector1).inverse
+    ) * self
+    return all(localProjection .>= 0)
+  }
+}
--- Tests/VectorTests.swift
+++ Tests/VectorTests.swift
@@ -3,14 +3,21 @@ import Testing
 
 struct VectorTests {
   @Test func isBetween() {
-    #expect(SIMD2.one.isBetween([1, 0], and: [0, 1]))
-    #expect(!(-SIMD2.one).isBetween([1, 0], and: [0, 1]))
-    #expect(SIMD2.one.isBetween([0, 1], and: [1, 0]))
-    #expect(!(SIMD2.one).isBetween([0, 1], and: [-1, 0]))
+    test(Float.self)
+    test(Double.self)
 
-    #expect(SIMD3.one.isBetween([1, 0, 0], and: [0, 1, 0]))
-    #expect(!(-SIMD3.one).isBetween([1, 0, 0], and: [0, 1, 0]))
-    #expect(SIMD3.one.isBetween([0, 1, 0], and: [1, 0, 0]))
-    #expect(!(SIMD3.one).isBetween([0, 1, 0], and: [-1, 0, 0]))
+    func test<Scalar: FloatingPointScalar>(_: Scalar.Type) {
+      typealias Vector2 = SIMD2<Scalar>
+      #expect(Vector2.one.isBetween([1, 0], and: [0, 1]))
+      #expect(!(-Vector2.one).isBetween([1, 0], and: [0, 1]))
+      #expect(Vector2.one.isBetween([0, 1], and: [1, 0]))
+      #expect(!(Vector2.one).isBetween([0, 1], and: [-1, 0]))
+
+      typealias Vector3 = SIMD3<Scalar>
+      #expect(Vector3.one.isBetween([1, 0, 0], and: [0, 1, 0]))
+      #expect(!(-Vector3.one).isBetween([1, 0, 0], and: [0, 1, 0]))
+      #expect(Vector3.one.isBetween([0, 1, 0], and: [1, 0, 0]))
+      #expect(!(Vector3.one).isBetween([0, 1, 0], and: [-1, 0, 0]))
+    }
   }
 }
